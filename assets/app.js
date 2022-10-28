5bce0d1617bb5437b86c9c366a220534cb5e1188
umayevg <beamyg@gmail.com>
2022-10-28 20:29:52 +0200

    Copy to clipboard added

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,7 @@ const cols = document.querySelectorAll(".col")
 
 // Listening for Space button
 document.addEventListener("keydown", event => {
+  event.preventDefault()
   if (event.code.toLowerCase() === "space") {
     getRandomColors()
   }
@@ -10,17 +11,33 @@ document.addEventListener("keydown", event => {
 document.addEventListener("click", event => {
   const type = event.target.dataset.type
   if (type === "lock") {
+    const node =
+      event.target.tagName.toLowerCase() === "i"
+        ? event.target
+        : event.target.children[0]
+
+    node.classList.toggle("fa-lock-open")
+    node.classList.toggle("fa-lock")
   }
 })
 
 // Function sets random color to the column
 function getRandomColors() {
   cols.forEach(col => {
-    const text = col.querySelector("h2")
+    const isLocked = col.querySelector("i").classList.contains("fa-lock")
+    const text = col.querySelector("h3")
     const color = generateRandomColor()
+
+    if (isLocked) {
+      return
+    }
     col.style.background = color
     text.innerText = color
   })
+}
+
+function copyToClipboard(text) {
+  return navigator.clipboard.writeText(text)
 }
 
 // Genrates random color
